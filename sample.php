8b531504b0cf2fb8f5b96df5d28bdf784ae6d286
Eric Nguyen <62949169+Eric-nguyen1402@users.noreply.github.com>
2021-12-17 13:56:22 +0800

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,5 @@
+<div class="col-xs-3 nopadding zoom" style = "align-self: flex-end;" id = "bot-1-line-camera3">
+    <div class = "col-xs-12">
+        <div class = "camera_id3" id = "1-line-camera_id3"></div>
+    </div>
+</div> <!-- div camera id 3 -->
