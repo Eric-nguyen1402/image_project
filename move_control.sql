686393054e603f46569473d79d0625e334af669d
Eric Nguyen <62949169+Eric-nguyen1402@users.noreply.github.com>
2022-01-07 08:41:33 +0800

    Add files via upload

--- move_control.sql
+++ move_control.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost:3306
--- Thời gian đã tạo: Th9 27, 2021 lúc 09:22 AM
--- Phiên bản máy phục vụ: 10.3.27-MariaDB-0+deb10u1
--- Phiên bản PHP: 7.3.19-1~deb10u1
+-- Thời gian đã tạo: Th1 07, 2022 lúc 08:40 AM
+-- Phiên bản máy phục vụ: 10.3.29-MariaDB-0+deb10u1
+-- Phiên bản PHP: 7.3.29-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,19 +31,22 @@ CREATE TABLE `move_control` (
   `level` bigint(20) NOT NULL DEFAULT 0,
   `requests_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `battery` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `current` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `error_can` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
   `ultrasonic` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT current_timestamp(),
-  `status_login` int(11) NOT NULL DEFAULT 0
+  `status_login` int(11) NOT NULL DEFAULT 0,
+  `led_level` bigint(20) NOT NULL DEFAULT 0,
+  `voltage` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `move_control`
 --
 
-INSERT INTO `move_control` (`id`, `level`, `requests_time`, `battery`, `error_can`, `ultrasonic`, `created_at`, `updated_at`, `status_login`) VALUES
-(1, 0, '1631838730679', '90', 'OK', '0', '2020-11-16 00:00:00', '2021-09-16 16:32:11', 1);
+INSERT INTO `move_control` (`id`, `level`, `requests_time`, `battery`, `current`, `error_can`, `ultrasonic`, `created_at`, `updated_at`, `status_login`, `led_level`, `voltage`) VALUES
+(1, 0, '1641455891047', '39', '274', 'OK', '0', '2020-11-16 00:00:00', '2022-01-05 23:58:11', 1, 0, '2.266');
 
 --
 -- Chỉ mục cho các bảng đã đổ
